af0d1693abd3763b8d2b65084df8ecfb15080b06
Yurbos <49271859+Yurbos@users.noreply.github.com>
2019-04-05 11:17:49 +0500

    Update py_ls_size_std.py

--- py_ls_size_std.py
+++ py_ls_size_std.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
+root_dir = sys.argv[1]
 
+print(root_dir)
 f = []
 d = []
 l = []
@@ -11,41 +13,59 @@ d_size = []
 l_size = []
 
 
-with os.scandir(sys.argv[1]) as i:
+def get_size(start_path):
+    total_size = 0
+    seen = {}
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for j in filenames:
+            fp = os.path.join(dirpath, j)
+            try:
+                stat = os.stat(fp)
+            except OSError:
+                continue
+            try:
+                seen[stat.st_ino]
+            except KeyError:
+                seen[stat.st_ino] = True
+            else:
+                continue
+            total_size += stat.st_size
+    return total_size
+
+
+def tofixed(numobj, digits):
+    return f"{numobj:.{digits}f}"
+
+
+def size_print(i):
+    if i >= 1073741824:
+        i = i / 1073741824
+        result = str(tofixed(i, 3))+" GB"
+        return result
+    else:
+        i = i / 1048576
+        result = str(tofixed(i, 3)) + " MB"
+        return result
+
+
+with os.scandir(root_dir) as i:
     for entry in i:
         if entry.is_file():
             f.append(entry.name)
             f_size.append(os.path.getsize(entry.path))
         if entry.is_dir():
             d.append(entry.name)
-
-            def get_size(start_path=entry.path):
-                total_size = 0
-                seen = {}
-                for dirpath, dirnames, filenames in os.walk(start_path):
-                    for j in filenames:
-                        fp = os.path.join(dirpath, j)
-                        try:
-                            stat = os.stat(fp)
-                        except OSError:
-                            continue
-                        try:
-                            seen[stat.st_ino]
-                        except KeyError:
-                            seen[stat.st_ino] = True
-                        else:
-                            continue
-                        total_size += stat.st_size
-                return total_size
-            d_size.append(get_size())
+            size = get_size(start_path=entry.path)
+            d_size.append(size)
         if entry.is_symlink():
             l.append(entry.name)
 
+
 for x in range(len(d)):
-    print("Dir\t", d_size[x],"\t", "\t", d[x])
+    print("Dir\t", size_print(d_size[x]),"\t", "\t", d[x])
 
 for x in range(len(f)):
-    print("File\t", f_size[x],"\t", "\t", f[x])
+    print("File\t", size_print(f_size[x]),"\t", "\t", f[x])
 
 for x in range(len(l)):
     print("Link\t", "\t", "\t", l[x])
