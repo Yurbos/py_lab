2898652e082dd750fe6c62110e95bab05473cb86
Yurbos <49271859+Yurbos@users.noreply.github.com>
2019-04-08 16:35:14 +0500

    Add files via upload

---
+++ db_con.py
@@ -0,0 +1,32 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+
+
+db_type = "postgres"
+db_server = "127.0.0.1"
+db_port = "5432"
+db_name = "test_db"
+db_user = "db_username"
+db_password = "db_password"
+
+
+db_string = '%s://%s:%s@%s:%s/%s' % (db_type, db_user, db_password, db_server, db_port, db_name)
+
+
+def test_con(test_string):
+    try:
+        db_test = create_engine(test_string)
+        db_test.connect()
+    except Exception:
+        print("Could not connect to server")
+        return 1
+    else:
+        print("All right, we here!")
+        return 0
+
+
+if test_con(db_string) == 1:
+    raise SystemExit
+
+
+print("stay here!")
